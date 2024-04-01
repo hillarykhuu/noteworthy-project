dd8e029c04efb00e1cf230c38ee8b236d1b9f86b
Hillary Khuu <hillarykhuu@berkeley.edu>
2024-04-01 16:51:22 -0700

    fixed video page

--- src/pages/FavoriteVideosPage/index.js
+++ src/pages/FavoriteVideosPage/index.js
@@ -10,41 +10,41 @@ import fsnote3 from "../../images/fsnote3.png"
 
 export default function FavoriteVideosPage() {
   return (
-    
-    
     <div id='FavoriteVideosDiv'>
       <h1 class='center'>Our Favorite Videos</h1>
       <Carousel data-bs-theme="dark" id = 'center-carousel'>
         <Carousel.Item>
-          <img
+          {/* <img
             className="d-block w-100"
             src={fsnote1}
             alt="first pics"
-          />
+          /> */}
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/n0p8MxT8AxI?si=zy1n0FfabweMSpAQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <Carousel.Caption>
-            <h5>Bet On It</h5>
+            <h5 id='carousel-heading'>Bet On It</h5>
             {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
+          {/* <img
             className="d-block w-100"
             src={fsnote2}
             alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h5> I'm Just Ken</h5>
+          /> */}
+<iframe width="560" height="315" src="https://www.youtube.com/embed/QHqUXFsKwZQ?si=mZsK5aTqHPPB3jNY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>          <Carousel.Caption>
+            <h5 id='carousel-heading'> I'm Just Ken</h5>
             {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
+          {/* <img
             className="d-block w-100"
             src={fsnote3}
             alt="Third slide"
-          />
+          /> */}
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/cDjY-1WuB3o?si=A_7nkO9-fkxPcf4Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <Carousel.Caption>
-            <h5>A Nightingale Sang in Berkeley Square</h5>
+            <h5 id='carousel-heading'>A Nightingale Sang in Berkeley Square</h5>
             {/* <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p> */}
